b7bef4c5e6bfed52f17810bb44f8ad7195425393
Seba <sebacampos012@gmail.com>
2017-09-14 19:02:20 +1000

    Added new keypoints

--- src/js/screens/Home.js
+++ src/js/screens/Home.js
@@ -156,7 +156,9 @@ class Home extends Component {
           <CarouselWidget title="the fitness, healthiness, happiness COLLECTIVE"  feed="carousel.1" />
           <FeatureBox feed="feature1"/>
           <CarouselWidget title="the fitness COLLECTIVE" feed="carousel.2" />
+          <FeatureBox feed="feature2"/>
           <CarouselWidget title="the food & fuel COLLECTIVE" feed="carousel.3"/>
+          <FeatureBox feed="feature3"/>
           <CarouselWidget title="the delivered diets COLLECTIVE" feed="carousel.4" />
           <CarouselWidget title="the mindfulness & mentor COLLECTIVE" feed="carousel.5" />
           <InstagramFeed title="Collective Bodies" feed="collectivebody" />
